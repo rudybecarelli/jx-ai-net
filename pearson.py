05d07a765b6eb27ce1ec050896a2c102894427ad
rudy.becarelli@gmail.com <rudy.becarelli@gmail.com>
2018-07-07 11:17:52 +0200

    Primo prototipo

---
+++ pearson.py
@@ -0,0 +1,26 @@
+import argparse
+import seaborn
+
+from matplotlib import pyplot
+import numpy as np
+import os
+from pandas import read_csv
+
+
+# Read args
+parser = argparse.ArgumentParser(description='Produces the Pearson''s correlation matrix for x.csv' )
+parser.add_argument('training_data_folder', help='Training data folder')
+training_data_folder = parser.parse_args().training_data_folder
+
+
+# Load x
+x = read_csv(os.path.join(training_data_folder, 'x.csv'), header=None, index_col=False).values.astype('float32')
+
+# Compute the Pearson's matrix
+cm = np.absolute(np.corrcoef(x.transpose()))
+
+np.savetxt("pearson.csv", cm, delimiter=",", fmt='%.2f')
+
+seaborn.heatmap(cm, square=True)
+
+pyplot.savefig('pearson.png')
